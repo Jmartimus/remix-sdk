fa989d39de150b5fe00de2ca6cb755d2daab7a06
Jonathan Martinez <jmartinez@bestow.com>
2023-02-15 15:36:52 -0600

    updated lib.

--- lib/server/createProvider.d.ts
+++ lib/server/createProvider.d.ts
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
-import { LDUser } from 'launchdarkly-node-server-sdk';
+import { LDOptions, LDUser } from 'launchdarkly-node-server-sdk';
 export declare let ldClient: any;
-declare const createProvider: (sdkKey: string, user: LDUser) => Promise<({ children }: {
+declare const createProvider: (sdkKey: string, user: LDUser, options: LDOptions | undefined) => Promise<({ children }: {
     children: ReactNode;
 }) => JSX.Element>;
 export default createProvider;
